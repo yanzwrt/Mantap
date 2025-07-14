d4d10694cac61f1cd004b8b41009d1a022d19168
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-15 00:20:10 +0700

    vpsroot.sh

--- vpsroot.sh
+++ vpsroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Mod By Rakha-VPN
 #echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
-wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/sshd_config;
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/yanzwrt/Mantap/main/sshd_config;
 systemctl restart sshd;
 clear;
 echo -e "Masukkan Password:";
